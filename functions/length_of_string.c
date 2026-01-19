6333ebf32f92ab214fcde750f444c99139519feb
Akshay Devrale <akshaydevrale@gmail.com>
2026-01-19 09:51:35 +0530

    Refactor lengthOfString function to remove size parameter

--- functions/length_of_string.c
+++ functions/length_of_string.c
@@ -2,20 +2,21 @@
 
 // Program to find length of a string without using built-in functions
 
-char lengthOfString(char [],int);
+char lengthOfString(char []);
 void main(){
 	char str[10];
 	printf("Enter string: ");
 	scanf("%s",str);
 	
-	int length=lengthOfString(str,10);
+	int length=lengthOfString(str);
 	printf("Length of string is: %d",length);
 }
 
-char lengthOfString(char str[],int size){
+char lengthOfString(char str[]){
 	int i=0;
 	while(str[i]!='\0'){
 		i++;
 	}
 	return i;
+
 }
